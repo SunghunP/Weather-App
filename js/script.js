460dfed2beef18fcddbbc6e0c9296c869afdc6e0
Joe Park <sunghun.josephp@gmail.com>
2022-03-22 21:45:55 -0700

    Test API call

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // API call example
 // https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key} 
+
+// check to see if the request console.logs the correct data.
+$.ajax("https://api.openweathermap.org/data/2.5/weather?q=Federal%20Way&appid=key")
+// What we want to happen after the request is finished
+.then((data) => {
+    console.log(data)
+})
+
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
