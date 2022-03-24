446b2b9e754e9eadb781bebd8c4dcaff268ceb5b
Joe Park <sunghun.josephp@gmail.com>
2022-03-24 12:18:12 -0700

    Add constant variables

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@
 // https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid={API key} 
 
 // check to see if the request console.logs the correct data.
-$.ajax("https://api.openweathermap.org/data/2.5/weather?q=Federal%20Way&appid=key")
-// What we want to happen after the request is finished
-.then((data) => {
-    console.log(data)
-})
+// $.ajax("https://api.openweathermap.org/data/2.5/weather?q=Federal%20Way&appid=key")
+// // What we want to happen after the request is finished
+// .then((data) => {
+//     console.log(data)
+// })
 
+// This is for the city name and country code api calls
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
+const API_KEY = console.API_KEY
+const $contentContainer = $('.content-container')
 
 const kelvinToFahrenheit = function (kelvin) {
     return parseInt((kelvin*(9/5))-459.67)
 }
-console.log(kelvinToFahrenheit(290))
